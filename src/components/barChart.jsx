b45cacdda9e30a3d4ec5c60988816bc32ecd5be4
CharlieAtki <164878103+CharlieAtki@users.noreply.github.com>
2024-10-28 07:12:49 +0000

    Added more graphs - Line and Bar charts

---
+++ src/components/barChart.jsx
@@ -0,0 +1,109 @@
+import {
+    BarChart,
+    Bar,
+    ResponsiveContainer,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from "recharts";
+
+// data for reference
+const testData = [
+    {
+        name: 'Jan',
+        product1: 4000,
+        product2: 2400
+    },
+    {
+        name: 'Feb',
+        product1: 3000,
+        product2: 2210
+    },
+    {
+        name: 'Mar',
+        product1: 2000,
+        product2: 2290
+    },
+    {
+        name: 'Apr',
+        product1: 2780,
+        product2: 2000
+    },
+    {
+        name: 'May',
+        product1: 1890,
+        product2: 2181
+    },
+    {
+        name: 'Jun',
+        product1: 2390,
+        product2: 2500
+    }
+]
+const BarChartComponent = () => {
+    return (
+        // PS made the graph responsive once it's been added into a container
+
+        // YAxis adds the YAxi label onto the graph
+        // XAXis adds the XAxi label onto the graph
+        // CartesianGrid adds a grid onto the graph, but "stokeDasharray" changes the grids visuals into hyphens
+        // Tooltip creates an additional UI element for when you hover over values in the graph
+        // Legend adds a key to the bottom on the graph. This provides additional information
+
+        // The 2 Area elements visualise the data provided onto the graph.
+        // Stroke and fill are for the colour of the visualised data on the graph
+        // StackId allows the visualised data to be added on top of one another.
+
+        //<ResponsiveContainer width="100%" height="100%">
+            <BarChart width={500} height={500} data={testData}>
+                <YAxis />
+                <XAxis dataKey="name" />
+                <CartesianGrid strokeDasharray="5 5" />
+                <Tooltip content={<CustomTooltip />} />
+                <Legend />
+
+                <Bar
+                    type="monotone"
+                    dataKey="product1"
+                    stroke="#2563eb"
+                    fill="#3b82f6"
+                    stackId="1"
+                />
+
+                <Bar
+                    type="monotone"
+                    dataKey="product2"
+                    stroke="#7c3aed"
+                    fill="#8b5cf6"
+                    stackId="1"
+                />
+            </BarChart>
+       // </ResponsiveContainer>
+    )
+};
+
+
+// Custom Tooltip - Changes the UI of the tooltip to make it more intuitive
+const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+        return (
+             <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
+                 <p className="text-medium text-lg text-white">{label}</p>
+                 <p className="text-sm text-blue-400">
+                    product 1:
+                    <span className="ml-2">${payload[0].value}</span>
+                 </p>
+                 <p className="text-sm text-indigo-400">
+                     Product 2:
+                     <span className="ml-2">${payload[1].value}</span>
+                 </p>
+             </div>
+        );
+    }
+};
+
+// exporting the component.
+// This allows it to be used multiple times across my application without needing the code to be rewritten.
+export default BarChartComponent;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import Hero from '../components/Hero';
 import React from "react";
 import AreaChartComponents from "../components/areaChart.jsx";
+import LineChartComponent from "../components/lineChart.jsx";
+import BarChartComponent from "../components/barChart.jsx";
 
 // This "page" is where the different React "components" will be incorporated together
 // "Hero" refers to function, which creates a banner.
@@ -8,7 +10,11 @@ const Dashboard = () => {
     return (
         <>
             <Hero title={"ERP System"} subtitle={"Tailored for retail businesses"} />
-            <AreaChartComponents />
+            <div className="flex space-x-8">
+                <AreaChartComponents />
+                <LineChartComponent />
+                <BarChartComponent />
+            </div>
         </>
     );
 };
