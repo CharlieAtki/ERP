fcdd2364d92da910470cf3b9b90bef287d977db9
CharlieAtki <164878103+CharlieAtki@users.noreply.github.com>
2024-12-08 13:25:35 +0000

    File structure updated

--- server/routes/customerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {
-    addCustomer, customerLogin, customerLogout,
-} from '../controllers/customerController.js';
-
-const router = express.Router();
-
-router.get('/customer-Logout', customerLogout);
-export default router;
